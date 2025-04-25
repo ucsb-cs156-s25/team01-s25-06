e61c38cdbdd02625c9b03567ba7f5662daf1e5d7
yarwinliu <yarwinliu@ucsb.edu>
2025-04-25 09:21:56 -0700

    menu item review files

--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -1,7 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.MenuItemReview;
-import edu.ucsb.cs156.example.entities.UCSBDate;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;

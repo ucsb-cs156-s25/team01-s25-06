91124c97981b444e7e039d07256adcfce911450c
Timothy Nguyen <timothytnguyen@ucsb.edu>
2025-04-25 05:00:22 -0700

    Please 100/100 mutation

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsController.java
@@ -1,6 +1,5 @@
 package edu.ucsb.cs156.example.controllers;
 
-
 import edu.ucsb.cs156.example.entities.UCSBOrganizations;
 import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,63 +17,57 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * This is a REST controller for UCSBDates
+ * This is a REST controller for UCSBOrganizations.
  */
 @Tag(name = "UCSBOrganizations")
 @RequestMapping("/api/ucsborganizations")
 @RestController
 @Slf4j
 public class UCSBOrganizationsController extends ApiController {
+
     @Autowired
     UCSBOrganizationsRepository ucsbOrganizationsRepository;
-
 
     /**
      * List all UCSB organizations
      * 
-     * @return an iterable of UCSBOrganization
+     * @return an iterable of UCSBOrganizations
      */
-    @Operation(summary= "List all ucsb organizations")
+    @Operation(summary = "List all UCSB organizations")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
-    public Iterable<UCSBOrganizations> allUCSBDates() {
+    public Iterable<UCSBOrganizations> allUCSBOrganizations() {
         Iterable<UCSBOrganizations> organizations = ucsbOrganizationsRepository.findAll();
         return organizations;
     }
-//     //  private String orgCode;
-//   private String orgTranslationShort;
-//   private String orgTranslation;
-//   private boolean inactive;
+
     /**
      * Create a new organization
      * 
-     * @param orgCode  The organization code
-     * @param orgTranslationShort  The organization's short translation
-     * @param orgTranslation Organizations full name
+     * @param orgCode organization code
+     * @param orgTranslationShort organization's short translation
+     * @param orgTranslation organizations full name
      * @param inactive Is the organization active or inactive?
      * @return the saved organization
      */
-    @Operation(summary= "Create a new organization")
+    @Operation(summary = "Create a new organization")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
     public UCSBOrganizations postUCSBOrganization(
-            @Parameter(name="orgCode") @RequestParam String orgCode,
-            @Parameter(name="orgTranslationShort") @RequestParam String orgTranslationShort,
-            @Parameter(name="orgTranslation") @RequestParam String orgTranslation,
-            @Parameter(name="inactive") @RequestParam boolean inactive)
+            @Parameter(name = "orgCode") @RequestParam String orgCode,
+            @Parameter(name = "orgTranslationShort") @RequestParam String orgTranslationShort,
+            @Parameter(name = "orgTranslation") @RequestParam String orgTranslation,
+            @Parameter(name = "inactive") @RequestParam boolean inactive)
             throws JsonProcessingException {
-
 
         UCSBOrganizations org = new UCSBOrganizations();
         org.setOrgCode(orgCode);
         org.setOrgTranslationShort(orgTranslationShort);
         org.setOrgTranslation(orgTranslation);
-        org.setInactive(inactive);
+        org.setInactive(Boolean.valueOf(inactive));  
         UCSBOrganizations savedOrg = ucsbOrganizationsRepository.save(org);
 
         return savedOrg;
     }
-
 }

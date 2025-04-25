1d054413bffb0e8382dc5e6509898454cf5b5981
Timothy Nguyen <timothytnguyen@ucsb.edu>
2025-04-24 17:12:09 -0700

    Made UCSB Controllers!

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsController.java
@@ -0,0 +1,90 @@
+package edu.ucsb.cs156.example.controllers;
+
+
+import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.UCSBOrganizations;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.UCSBDateRepository;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a REST controller for UCSBDates
+ */
+@Tag(name = "UCSBOrganizations")
+@RequestMapping("/api/ucsborganizations")
+@RestController
+@Slf4j
+public class UCSBOrganizationsController extends ApiController {
+    @Autowired
+    UCSBOrganizationsRepository ucsbOrganizationsRepository;
+
+
+    /**
+     * List all UCSB organizations
+     * 
+     * @return an iterable of UCSBOrganization
+     */
+    @Operation(summary= "List all ucsb organizations")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<UCSBOrganizations> allUCSBDates() {
+        Iterable<UCSBOrganizations> organizations = ucsbOrganizationsRepository.findAll();
+        return organizations;
+    }
+//     //  private String orgCode;
+//   private String orgTranslationShort;
+//   private String orgTranslation;
+//   private boolean inactive;
+    /**
+     * Create a new organization
+     * 
+     * @param orgCode  The organization code
+     * @param orgTranslationShort  The organization's short translation
+     * @param orgTranslation Organizations full name
+     * @param inactive Is the organization active or inactive?
+     * @return the saved organization
+     */
+    @Operation(summary= "Create a new organization")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public UCSBOrganizations postUCSBOrganization(
+            @Parameter(name="orgCode") @RequestParam String orgCode,
+            @Parameter(name="orgTranslationShort") @RequestParam String orgTranslationShort,
+            @Parameter(name="orgTranslation") @RequestParam String orgTranslation,
+            @Parameter(name="inactive") @RequestParam String inactive)
+            throws JsonProcessingException {
+
+
+        UCSBOrganizations org = new UCSBOrganizations();
+        org.setOrgCode(orgCode);
+        org.setOrgTranslationShort(orgTranslationShort);
+        org.setOrgTranslation(orgTranslation);
+        org.setInactive(Boolean.parseBoolean(inactive));
+        UCSBOrganizations savedOrg = ucsbOrganizationsRepository.save(org);
+
+        return savedOrg;
+    }
+
+}
